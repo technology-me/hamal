d4efe39ae352090de5a5b31540961237bb38e34e
technology-me <lishangmingxuan@foxmail.com>
2020-12-19 12:05:01 +0800

    FIT
    How time flies.I find that My Program has had Bugs for a long time.And now Numpy can't fit Windows10 V2004.So I changed some codes to replace numpy.The method 'replace' have no codes to support it,but I can't find the version that have code can surppot it.So I deleted it.I hope that it can help you.
    Technology-me,China

--- demo.py
+++ demo.py
@@ -1,13 +1,15 @@
-import mushroom
+import hamal
 import time
-import numpy
+#import numpy
 path = 'demo.json'
 time1 = time.time()
 time2 = time.time()
 
+def average(l):
+    return float(sum(l))/len(l)
 
-file = mushroom.mushroom(path)
-test = mushroom.mushroom('test.json')
+file = hamal.Hamal(path)
+test = hamal.Hamal('test.json')
 def main():
     times = []
     print(file.size())
@@ -15,17 +17,17 @@ def main():
     print(file)
     file.delete(all)
 
-    mushroom.delete(path,all)
+    hamal.delete(path,all)
     for i in range(50):
         for j in range(50):
             time1 = time.time()
             file.write_sheet('put', i, j, i*2+j*2)
             time2 = time.time()
             times.append(time2-time1)
-    print('表平均写入时间：'+str(numpy.mean(times)))
-    times[0] = numpy.mean(times)
+    print('表平均写入时间：'+str(average(times)))
+    times[0] = average(times)
     times.append(test.read('sheet_write'))
-    test.write('sheet_write',numpy.mean(times))
+    test.write('sheet_write',average(times))
     times = []
     for i in range(50):
         for j in range(50):
@@ -33,42 +35,42 @@ def main():
             file.read_sheet('put', i,j)
             time2 = time.time()
             times.append(time2-time1)
-    print('表平均读取时间：'+str(numpy.mean(times)))
-    times[0] = numpy.mean(times)
+    print('表平均读取时间：'+str(average(times)))
+    times[0] = average(times)
     times.append(test.read('sheet_read'))
-    test.write('sheet_read',numpy.mean(times))
+    test.write('sheet_read',average(times))
     times = []
     for i in range(2500):
         time1 = time.time()
         file.write('me',i)
         time2 = time.time()
         times.append(time2-time1)
-    print('字平均写入时间：'+str(numpy.mean(times)))
-    times[0] = numpy.mean(times)
+    print('字平均写入时间：'+str(average(times)))
+    times[0] = average(times)
     times.append(test.read('write'))
-    test.write('write',numpy.mean(times))
+    test.write('write',average(times))
     times = []
     for i in range(2500):
         time1 = time.time()
         file.read('me')
         time2 = time.time()
         times.append(time2-time1)
-    print('字平均读取时间：'+str(numpy.mean(times)))
-    times[0] = numpy.mean(times)
+    print('字平均读取时间：'+str(average(times)))
+    times[0] = average(times)
     times.append(test.read('read'))
-    test.write('read',numpy.mean(times))
+    test.write('read',average(times))
     times = []
+    """
     for i in range(2500):
         time1 = time.time()
         file.replace('me','a',2500)
         time2 = time.time()
         times.append(time2-time1)
-    print('字平均替换时间：'+str(numpy.mean(times)))
-    times[0] = numpy.mean(times)
+    print('字平均替换时间：'+str(average(times)))
+    times[0] = average(times)
     times.append(test.read('replace'))
-    test.write('replace',numpy.mean(times))
+    test.write('replace',average(times))
+    """
 
 
-file.decode()
 main()
-file.encode()
